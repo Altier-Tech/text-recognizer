05dfb6aec4badbff0876ed52a1068cefa2ef999e
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-06-27 01:30:40 +0530

    Update recognizer.py

--- recognizer.py
+++ recognizer.py
@@ -1,3 +1,8 @@
-import easyocr
-reader = easyocr.Reader(['en'])
-result = reader.readtext('input/test.png')
+import cv2
+import pytesseract
+
+img = cv2.imread('input/test.png')
+
+# Adding custom options
+custom_config = r'--oem 3 --psm 6'
+pytesseract.image_to_string(img, config=custom_config)
